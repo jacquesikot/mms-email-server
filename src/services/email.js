19978f5a577b1e8336541b6f4004ca35cce24d30
Jacques Mac <mac@Jacques-MacBook-Pro.local>
2021-07-12 18:15:37 +0100

    feat: update email template

--- src/services/email.js
+++ src/services/email.js
@@ -4,16 +4,6 @@ const handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
 
-// const readHTMLFile = (path, callback) => {
-//   fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       callback(null, html);
-//     }
-//   });
-// };
-
 const OAuth2 = google.auth.OAuth2;
 
 const createTransporter = async () => {
@@ -53,7 +43,7 @@ const createTransporter = async () => {
 
 const sendMail = async (data) => {
   const filePath = path.join(
-    '/Users/mac/Documents/code/mms-email-server/src//templates/index.html'
+    '/Users/mac/Documents/code/mms-email-server/src/templates/index.html'
   );
   const source = fs.readFileSync(filePath, 'utf-8').toString();
   const template = handlebars.compile(source);
@@ -67,6 +57,13 @@ const sendMail = async (data) => {
     to: data.recipient,
     subject: 'Welcome to Mac Music School',
     html: htmlToSend,
+    attachments: [
+      {
+        filename: 'logo.png',
+        path: '/Users/mac/Documents/code/mms-email-server/src/templates/images/logo.png',
+        cid: 'logo',
+      },
+    ],
   };
 
   try {
@@ -78,32 +75,3 @@ const sendMail = async (data) => {
 };
 
 module.exports = sendMail;
-
-// readHTMLFile(
-//     '/Users/mac/Documents/code/mms-email-server/src//templates/index.html',
-//     async (err, html) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         const template = handlebars.compile(html);
-//         const replacements = {
-//           username: data.username,
-//         };
-//         const htmlToSend = template(replacements);
-
-//         const mailOptions = {
-//           from: process.GOOGLE_MAIL,
-//           to: data.recipient,
-//           subject: 'Welcome to Mac Music School',
-//           html: htmlToSend,
-//         };
-
-//         try {
-//           const emailTransporter = await createTransporter();
-//           await emailTransporter.sendMail(mailOptions);
-//         } catch (error) {
-//           console.log(error);
-//         }
-//       }
-//     }
-//   );
